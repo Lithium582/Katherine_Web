7858aa19a5c97f673ae8bcb79252999e9f92dbbf
lithium582 <angelmamberto15@hotmail.com>
2022-03-08 20:07:52 -0300

    Primer Commit
    Incluye ABM de libros y Login en la página de home
    Terminada conexión a la Base de Datos (con métodos de llamada a los Stored Procedures)
    
    La idea de este proyecto es no usar Entity Framework ni ninguna herramienta del estilo
    
    Pendientes:
    - Modificaciones en las funciones callbacks (promesas)
    - Pantalla de Login independiente
    - Acceso a las APIs usando el token JWT generado
    - Agregar estilos usando Tailwind
    - Generar consola de pruebas
    - Creación de tests unitarios
    - Limpiar páginas generadas automáticamente

---
+++ Katherine_API/Controllers/Autenticacion/AutenticacionController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosYPermisos;
+
+namespace Katherine_API.Controllers.Autenticacion
+{
+    [Route("~/[controller]")]
+    [ApiController]
+    public class AutenticacionController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AutenticacionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //El gusanito permite sobreescribir la ruta raíz
+        [Route("Login")]
+        [HttpPut]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = Usuario.Login(model.Usuario, model.Password);
+            if (user != null)
+            {
+                //var userRoles = await userManager.GetRolesAsync(user);
+
+                //var authClaims = new List<Claim>
+                //{
+                //    new Claim(ClaimTypes.Name, user.UserName),
+                //    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                //};
+
+                //foreach (var userRole in userRoles)
+                //{
+                //    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                //}
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(3),
+                    //claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ Katherine_API/Startup.cs
@@ -0,0 +1,124 @@
+using AccesoADatos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosYPermisos;
+
+namespace Katherine_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            //Carga de la configuración de la Base de datos
+            Configuracion configuracion = new Configuracion();
+            //Configuration.GetSection("JWT").Bind(configuracion);
+            Configuration.GetSection("BaseDatos").Bind(configuracion);
+
+            IniciarConexionBD(configuracion);
+        }
+
+        /// <summary>
+        /// Se inicia la conexión al inciar la aplicación
+        /// </summary>
+        private void IniciarConexionBD(Configuracion config)
+        {
+            BaseConexion.InicializarConexion(config);
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<Configuracion>(Configuration.GetSection("BaseDatos"));
+            services.AddControllers();
+
+            //Información sobre la API
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Katherine_API", Version = "v1" });
+            });
+
+            //CORS
+            services.AddCors(options =>
+            {   
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("https://localhost:44496").AllowAnyHeader().AllowAnyMethod();
+                    });
+            });
+
+            //Seguridad
+            services.AddIdentity<Usuario, IdentityRole>()
+                .AddDefaultTokenProviders();
+
+            //Autenticación
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["JWT:ValidAudience"],
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Katherine_API v1"));
+            }
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
